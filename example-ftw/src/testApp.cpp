48806089618be228dcec29339a6d6aa79643cac7
obviousjim <james@jamesgeorge.org>
2011-12-03 19:28:55 -0500

    ftgl ftw
    Signed-off-by: obviousjim <james@jamesgeorge.org>

---
+++ example-ftw/src/testApp.cpp
@@ -0,0 +1,108 @@
+/*
+ *  ofxWordPalette
+ *
+ * Created by James George, http://www.jamesgeorge.org @ Flightphase http://www.flightphase.com 
+ * for the National Maritime Musuem
+ * requires ofxFTGL : https://github.com/Flightphase/ofxFTGL
+ *
+ **********************************************************
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ----------------------
+ * ofxWordPalette lets you draw lots and lots of text efficiently by rendering
+ * a set number of words once into an FBO and then drawing them as textures
+ *
+ * ofxWordPalette also has helper functions to do fun stuff involving the length
+ * of words
+ */
+
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	ofSetFrameRate(30);	
+	ofSetVerticalSync(true);
+	
+	ofEnableAlphaBlending();
+	str = "FTGL FTW";
+	font.loadFont("verdana.ttf", ofGetHeight(), true);
+	ofSetWindowShape(1024, font.getStringBoundingBox(str, 0, 0).height);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofBackground(255);
+	ofSetColor(0);
+	font.drawString(str, ofGetWidth() - ( (ofGetFrameNum()*30) % int(font.getStringBoundingBox(str, 0,0).width + ofGetWidth())), ofGetHeight());
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ example-ftw/src/testApp.h
@@ -0,0 +1,65 @@
+/*
+ *  ofxWordPalette
+ *
+ * Created by James George, http://www.jamesgeorge.org @ Flightphase http://www.flightphase.com 
+ * for the National Maritime Musuem
+ * requires ofxFTGL : https://github.com/Flightphase/ofxFTGL
+ *
+ **********************************************************
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ----------------------
+ * ofxWordPalette lets you draw lots and lots of text efficiently by rendering
+ * a set number of words once into an FBO and then drawing them as textures
+ *
+ * ofxWordPalette also has helper functions to do fun stuff involving the length
+ * of words
+ */
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxFTGLFont.h"
+
+class testApp : public ofBaseApp{
+
+  public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofxFTGLFont font;
+	string str;
+	
+	vector<ofVec2f> points;
+};
